8019e8b372d64ffca6d7b3af5eb02b3bb660da17
hanzhao2020 <hazhao@eng.ucsd.edu>
2023-03-24 20:28:38 -0700

    Updating the visualization script.

--- visualization/README.md
+++ visualization/README.md
@@ -6,7 +6,7 @@ pvpython --version
 
 Run ``pvpython view_results.py --help`` to see available options. For example, ``start_ind`` and ``end_ind`` are the first and last spline indices for the saved results. ``file_path`` is the location where the results are saved. By default, this script only displays the deformed geometry scaled by ``disp_scale``, whose default value is 1. Users can choose to show the unformed geometry by setting ``show_geom=True`` or von Mises stress (if computed and saved) using ``show_stress=True``. To launch the interactive window, set ``interact=True``.
 
-Running ``pvpython view_results.py`` will generate a Paraview state file by default in the ``states`` directory, the filename is created in the order: "``filename_prefix``\_patch_``start_ind``\_``end_ind``\_disp``disp_scale``.pvsm". Then load the state file into Paraview and click the reset button to view the results. Users can also manually modify the default options, e.g., ``file_path``, in the script and run it in Paraview's Python shell.
+Running ``pvpython view_results.py`` will generate a Paraview state file by default in the ``states`` directory, the filename is created in the order: "``filename_prefix``\_patch_``start_ind``\_``end_ind``\_disp_dispscale``disp_scale``.pvsm". Then load the state file into Paraview and click the reset button to view the results. Users can also manually modify the default options, e.g., ``file_path``, in the script and run it in Paraview's Python shell.
 
 Example:
 
--- visualization/view_results.py
+++ visualization/view_results.py
@@ -1,5 +1,4 @@
 # trace generated using paraview version 5.9.0
-
 import os
 import argparse
 import distutils
@@ -11,32 +10,45 @@ from paraview.simple import *
 paraview.simple._DisableFirstRenderCameraReset()
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--file_path', dest='file_path', default='./',
+parser.add_argument('--file_path', dest='file_path', default='./results/',
                     help="File path for saved results.")
+# For geometry
 parser.add_argument('--show_geom', dest='show_geom', default=False, 
                     help="Show undeformed geometry.")
 parser.add_argument('--show_geom_edge', dest='show_geom_edge', default=False, 
                     help="Show edges on undeformed geometry.")
-parser.add_argument('--show_disp', dest='show_disp', default=True, 
+parser.add_argument('--geom_opacity', dest='geom_opacity', default=1.0,
+                    help="Opacity for undeformed geometry.")
+# For displacements
+parser.add_argument('--show_disp', dest='show_disp', default=False, 
                     help="Show deformed geometry.")
 parser.add_argument('--show_disp_edge', dest='show_disp_edge', default=False,
                     help="Show edges on deformed geometry.")
-parser.add_argument('--show_stress', dest='show_stress', default=False,
+parser.add_argument('--disp_opacity', dest='disp_opacity', default=1.0,
+                    help="Opacity for deformed geometry.")
+parser.add_argument('--disp_scale', dest='disp_scale', default=1.0,
+                    help="Scale factor for displacement.")
+# For von Mises stress
+parser.add_argument('--show_stress', dest='show_stress', default=True,
                     help="Show von Mises stress on deformed geometry.")
 parser.add_argument('--show_stress_edge', dest='show_stress_edge', default=False,
                     help="Show edges on von Mises stress.")
-parser.add_argument('--geom_opacity', dest='geom_opacity', default=1.0,
-                    help="Opacity for undeformed geometry.")
-parser.add_argument('--disp_opacity', dest='disp_opacity', default=1.0,
-                    help="Opacity for deformed geometry.")
-parser.add_argument('--disp_scale', dest='disp_scale', default=1,
-                    help="Scale factor for displacement.")
 parser.add_argument('--stress_opacity', dest='stress_opacity', default=1.0,
                     help="Opacity for stress.")
+# For thickness
+parser.add_argument('--show_thickness', dest='show_thickness', default=False,
+                    help="Show shell thickness on deformed geometry")
+parser.add_argument('--show_thickness_edge', dest='show_thickness_edge', default=False,
+                    help="Show edges on thickness")
+parser.add_argument('--thickness_opacity', dest='thickness_opacity', default=1.0,
+                    help="Opacity for thickness.")
+# For surface indices
 parser.add_argument('--start_ind', dest='start_ind', default=0,
                     help="Index for first spline.")
 parser.add_argument('--end_ind', dest='end_ind', default=2,
                     help="Index for last spline.")
+parser.add_argument('--ind_step', dest='ind_step', default=1,
+                    help="Step for surface indices.")
 parser.add_argument('--save_state', dest='save_state', default=True,
                     help="Save current view to a Paraview state file.")
 parser.add_argument('--filename_prefix', dest='filename_prefix', default='state',
@@ -45,138 +57,171 @@ parser.add_argument('--interact', dest='interact', default=False,
                     help="Launch interactive window.")
 
 args = parser.parse_args()
-
 true_list = ['true', 'yes', 'y', '1']
 
 ### Arguments
 file_path = args.file_path
-
+# For geometry
 show_geom = str(args.show_geom).lower() in true_list
 show_geom_edge = str(args.show_geom_edge).lower() in true_list
-
+geom_opacity = float(args.geom_opacity)
+# For displacements
 show_disp = str(args.show_disp).lower() in true_list
 show_disp_edge = str(args.show_disp_edge).lower() in true_list
-
-show_stress = str(args.show_stress).lower() in true_list
-show_stress_edge = str(args.show_stress_edge).lower() in true_list
-
-geom_opacity = float(args.geom_opacity)
 disp_opacity = float(args.disp_opacity)
 disp_scale = int(args.disp_scale)
+# For stress
+show_stress = str(args.show_stress).lower() in true_list
+show_stress_edge = str(args.show_stress_edge).lower() in true_list
 stress_opacity = float(args.stress_opacity)
-
+# For thickness
+show_thickness = str(args.show_thickness).lower() in true_list
+show_thickness_edge = str(args.show_thickness_edge).lower() in true_list
+thickness_opacity = float(args.thickness_opacity)
+# For surface indices
 start_ind = int(args.start_ind)
 end_ind = int(args.end_ind)
-num_surf = end_ind - start_ind
+ind_step = int(args.ind_step)
+# # To use custom indices
+# custom_inds = [0,1,2,3,4]
 
 save_state = str(args.save_state).lower() in true_list
 filename = args.filename_prefix
 interact = str(args.interact).lower() in true_list
-
 
 if save_state:
     filename += "_patch_"+str(start_ind)+"_"+str(end_ind)
     if show_geom:
         filename += "_geom"
     if show_disp:
-        filename += "_disp"+str(disp_scale)
+        filename += "_disp"
     if show_stress:
         filename += "_stress"
+    if show_thickness:
+        filename += "_thickness"
+    filename += "_dispscale"+str(disp_scale)
     filename += ".pvsm"
 
 renderView1 = GetActiveViewOrCreate('RenderView')
 
-for surf_ind in range(start_ind, end_ind):
+geom_fname_pre = "F"
+disp_fname_pre = "u"
+stress_fname_pre = "von_Mises_top_"
+thickness_fname_pre = "t"
 
+for surf_ind in range(start_ind, end_ind, ind_step):
+# for surf_ind in custom_inds:
     print("--- Displaying surface", surf_ind)
 
     # create a new 'PVD Reader'
-    f0_0_filepvd = PVDReader(registrationName='F'+str(surf_ind)+'_0_file.pvd', 
-                             FileName=file_path+'F'+str(surf_ind)+'_0_file.pvd')
-    f0_0_filepvd.PointArrays = ['F'+str(surf_ind)+'_0']
+    f0_0_filepvd = PVDReader(registrationName=geom_fname_pre+str(surf_ind)+'_0_file.pvd', 
+                             FileName=file_path+geom_fname_pre+str(surf_ind)+'_0_file.pvd')
+    f0_0_filepvd.PointArrays = [geom_fname_pre+str(surf_ind)+'_0']
 
     # create a new 'PVD Reader'
-    f0_1_filepvd = PVDReader(registrationName='F'+str(surf_ind)+'_1_file.pvd', 
-                             FileName=file_path+'F'+str(surf_ind)+'_1_file.pvd')
-    f0_1_filepvd.PointArrays = ['F'+str(surf_ind)+'_1']
+    f0_1_filepvd = PVDReader(registrationName=geom_fname_pre+str(surf_ind)+'_1_file.pvd', 
+                             FileName=file_path+geom_fname_pre+str(surf_ind)+'_1_file.pvd')
+    f0_1_filepvd.PointArrays = [geom_fname_pre+str(surf_ind)+'_1']
 
     # create a new 'PVD Reader'
-    f0_2_filepvd = PVDReader(registrationName='F'+str(surf_ind)+'_2_file.pvd', 
-                             FileName=file_path+'F'+str(surf_ind)+'_2_file.pvd')
-    f0_2_filepvd.PointArrays = ['F'+str(surf_ind)+'_2']
+    f0_2_filepvd = PVDReader(registrationName=geom_fname_pre+str(surf_ind)+'_2_file.pvd', 
+                             FileName=file_path+geom_fname_pre+str(surf_ind)+'_2_file.pvd')
+    f0_2_filepvd.PointArrays = [geom_fname_pre+str(surf_ind)+'_2']
 
     # create a new 'PVD Reader'
-    f0_3_filepvd = PVDReader(registrationName='F'+str(surf_ind)+'_3_file.pvd', 
-                             FileName=file_path+'F'+str(surf_ind)+'_3_file.pvd')
-    f0_3_filepvd.PointArrays = ['F'+str(surf_ind)+'_3']
+    f0_3_filepvd = PVDReader(registrationName=geom_fname_pre+str(surf_ind)+'_3_file.pvd', 
+                             FileName=file_path+geom_fname_pre+str(surf_ind)+'_3_file.pvd')
+    f0_3_filepvd.PointArrays = [geom_fname_pre+str(surf_ind)+'_3']
 
     # create a new 'PVD Reader'
-    u0_0_filepvd = PVDReader(registrationName='u'+str(surf_ind)+'_0_file.pvd', 
-                             FileName=file_path+'u'+str(surf_ind)+'_0_file.pvd')
-    u0_0_filepvd.PointArrays = ['u'+str(surf_ind)+'_0']
+    u0_0_filepvd = PVDReader(registrationName=disp_fname_pre+str(surf_ind)+'_0_file.pvd', 
+                             FileName=file_path+disp_fname_pre+str(surf_ind)+'_0_file.pvd')
+    u0_0_filepvd.PointArrays = [disp_fname_pre+str(surf_ind)+'_0']
 
     # create a new 'PVD Reader'
-    u0_1_filepvd = PVDReader(registrationName='u'+str(surf_ind)+'_1_file.pvd', 
-                             FileName=file_path+'u'+str(surf_ind)+'_1_file.pvd')
-    u0_1_filepvd.PointArrays = ['u'+str(surf_ind)+'_1']
+    u0_1_filepvd = PVDReader(registrationName=disp_fname_pre+str(surf_ind)+'_1_file.pvd', 
+                             FileName=file_path+disp_fname_pre+str(surf_ind)+'_1_file.pvd')
+    u0_1_filepvd.PointArrays = [disp_fname_pre+str(surf_ind)+'_1']
 
     # create a new 'PVD Reader'
-    u0_2_filepvd = PVDReader(registrationName='u'+str(surf_ind)+'_2_file.pvd', 
-                             FileName=file_path+'u'+str(surf_ind)+'_2_file.pvd')
-    u0_2_filepvd.PointArrays = ['u'+str(surf_ind)+'_2']
+    u0_2_filepvd = PVDReader(registrationName=disp_fname_pre+str(surf_ind)+'_2_file.pvd', 
+                             FileName=file_path+disp_fname_pre+str(surf_ind)+'_2_file.pvd')
+    u0_2_filepvd.PointArrays = [disp_fname_pre+str(surf_ind)+'_2']
 
     if show_stress:
         # create a new 'PVD Reader'
-        von_Mises_top_0pvd = PVDReader(registrationName='von_Mises_top_'+str(surf_ind)+'.pvd', 
-                                       FileName=file_path+'von_Mises_top_'+str(surf_ind)+'.pvd')
-        von_Mises_top_0pvd.PointArrays = ['von_Mises_top_'+str(surf_ind)+'']
+        von_Mises_0pvd = PVDReader(registrationName=stress_fname_pre+str(surf_ind)+'.pvd', 
+                                       FileName=file_path+stress_fname_pre+str(surf_ind)+'.pvd')
+        von_Mises_0pvd.PointArrays = [stress_fname_pre+str(surf_ind)]
+
+    if show_thickness:
+        t0_filepvd = PVDReader(registrationName=thickness_fname_pre+str(surf_ind)+'_file.pvd',
+                               FileName=file_path+thickness_fname_pre+str(surf_ind)+'_file.pvd')
+        t0_filepvd.PointArrays = [thickness_fname_pre+str(surf_ind)]
 
     # set active source
     SetActiveSource(f0_0_filepvd)
 
     # # set active source
-    # SetActiveSource(von_Mises_top_0pvd)
+    # SetActiveSource(von_Mises_0pvd)
+
+    appendAttributes_input = [f0_0_filepvd, f0_1_filepvd, f0_2_filepvd, f0_3_filepvd, 
+                              u0_0_filepvd, u0_1_filepvd, u0_2_filepvd]
 
     if show_stress:
-        # create a new 'Append Attributes'
-        appendAttributes1 = AppendAttributes(registrationName='AppendAttributes'+str(surf_ind), 
-                                             Input=[f0_0_filepvd, f0_1_filepvd, f0_2_filepvd, f0_3_filepvd, 
-                                                    u0_0_filepvd, u0_1_filepvd, u0_2_filepvd, von_Mises_top_0pvd])
-    else:
-        appendAttributes1 = AppendAttributes(registrationName='AppendAttributes'+str(surf_ind), 
-                                             Input=[f0_0_filepvd, f0_1_filepvd, f0_2_filepvd, f0_3_filepvd, 
-                                                    u0_0_filepvd, u0_1_filepvd, u0_2_filepvd])
+        appendAttributes_input += [von_Mises_0pvd]
+    if show_thickness:
+        appendAttributes_input += [t0_filepvd]
+
+    appendAttributes1 = AppendAttributes(registrationName='AppendAttributes'+str(surf_ind), 
+                                         Input = appendAttributes_input)
 
     # create a new 'Calculator'
-    calculator1 = Calculator(registrationName='Calculator'+str(surf_ind)+'_1', Input=appendAttributes1)
+    calculator1 = Calculator(registrationName='CalculatorGeom'+str(surf_ind), 
+                             Input=appendAttributes1)
 
     # Properties modified on calculator1
-    calculator1.Function = '(F'+str(surf_ind)+'_0/F'+str(surf_ind)+'_3-coordsX)*iHat + (F'+str(surf_ind)+'_1/F'+str(surf_ind)+'_3-coordsY)*jHat + (F'+str(surf_ind)+'_2/F'+str(surf_ind)+'_3-coordsZ)*kHat'
+    calculator1.Function = '('+geom_fname_pre+str(surf_ind)+'_0/'\
+                         +geom_fname_pre+str(surf_ind)+'_3-coordsX)*iHat '\
+                        +'+ ('+geom_fname_pre+str(surf_ind)+'_1/'\
+                         +geom_fname_pre+str(surf_ind)+'_3-coordsY)*jHat '\
+                        +'+ ('+geom_fname_pre+str(surf_ind)+'_2/'\
+                         +geom_fname_pre+str(surf_ind)+'_3-coordsZ)*kHat'
 
     # create a new 'Warp By Vector'
-    warpByVector1 = WarpByVector(registrationName='WarpByVector'+str(surf_ind)+'_1', Input=calculator1)
+    warpByVector1 = WarpByVector(registrationName='WarpByVectorGeom'+str(surf_ind), 
+                                 Input=calculator1)
     warpByVector1.Vectors = ['POINTS', 'Result']
 
     if show_geom:
-        warpByVector1Display = Show(warpByVector1, renderView1, 'UnstructuredGridRepresentation')
+        warpByVector1Display = Show(warpByVector1, renderView1, 
+                                    'UnstructuredGridRepresentation')
         ColorBy(warpByVector1Display, None)
         warpByVector1Display.Opacity = geom_opacity
         if show_geom_edge:
             warpByVector1Display.SetRepresentationType('Surface With Edges')
 
     # create a new 'Calculator'
-    calculator2 = Calculator(registrationName='Calculator'+str(surf_ind)+'_2', Input=warpByVector1)
+    calculator2 = Calculator(registrationName='CalculatorDisp'+str(surf_ind), 
+                             Input=warpByVector1)
 
     # Properties modified on calculator2
-    calculator2.Function = '(u'+str(surf_ind)+'_0/F'+str(surf_ind)+'_3)*iHat + (u'+str(surf_ind)+'_1/F'+str(surf_ind)+'_3)*jHat + (u'+str(surf_ind)+'_2/F'+str(surf_ind)+'_3)*kHat'
+    calculator2.Function = '('+disp_fname_pre+str(surf_ind)+'_0/'\
+                         +geom_fname_pre+str(surf_ind)+'_3)*iHat '\
+                        +'+ ('+disp_fname_pre+str(surf_ind)+'_1/'\
+                         +geom_fname_pre+str(surf_ind)+'_3)*jHat '\
+                        +'+ ('+disp_fname_pre+str(surf_ind)+'_2/'\
+                         +geom_fname_pre+str(surf_ind)+'_3)*kHat'
 
     # create a new 'Warp By Vector'
-    warpByVector2 = WarpByVector(registrationName='WarpByVector'+str(surf_ind)+'_2', Input=calculator2)
+    warpByVector2 = WarpByVector(registrationName='WarpByVectorDisp'+str(surf_ind), 
+                                 Input=calculator2)
     warpByVector2.Vectors = ['POINTS', 'Result']
     warpByVector2.ScaleFactor = disp_scale
     
     if show_disp:
-        warpByVector2Display = Show(warpByVector2, renderView1, 'UnstructuredGridRepresentation')
+        warpByVector2Display = Show(warpByVector2, renderView1, 
+                                    'UnstructuredGridRepresentation')
         ColorBy(warpByVector2Display, ('POINTS', 'Result', 'Magnitude'))
         warpByVector2Display.Opacity = disp_opacity
         if show_disp_edge:
@@ -189,16 +234,33 @@ for surf_ind in range(start_ind, end_ind):
 
     if show_stress:
         # create a new 'Calculator'
-        calculator3 = Calculator(registrationName='Calculator'+str(surf_ind)+'_3', Input=warpByVector2)
+        calculator3 = Calculator(registrationName='CalculatorvM'+str(surf_ind), 
+                                 Input=warpByVector2)
 
         # Properties modified on calculator3
-        calculator3.Function = 'von_Mises_top_'+str(surf_ind)+''
+        calculator3.Function = stress_fname_pre+str(surf_ind)
 
         # UpdatePipeline(time=0.0, proxy=calculator3)
-        calculator3Display = Show(calculator3, renderView1, 'UnstructuredGridRepresentation')
+        calculator3Display = Show(calculator3, renderView1, 
+                                  'UnstructuredGridRepresentation')
         calculator3Display.Opacity = stress_opacity
         if show_stress_edge:
             calculator3Display.SetRepresentationType('Surface With Edges')
+
+    if show_thickness:
+        # create a new 'Calculator'
+        calculator4 = Calculator(registrationName='CalculatorTh'+str(surf_ind), 
+                                 Input=warpByVector2)
+
+        # Properties modified on calculator4
+        calculator4.Function = thickness_fname_pre+str(surf_ind)
+
+        # UpdatePipeline(time=0.0, proxy=calculator4)
+        calculator4Display = Show(calculator4, renderView1, 
+                                  'UnstructuredGridRepresentation')
+        calculator4Display.Opacity = thickness_opacity
+        if show_thickness_edge:
+            calculator4Display.SetRepresentationType('Surface With Edges')
 
 
 renderView1.InteractionMode = '3D'
@@ -213,3 +275,4 @@ if save_state:
 if interact:
     RenderAllViews()
     Interact()
+exit()
